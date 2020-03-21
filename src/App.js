81ee8f0853dbefb91d599a52babfd0b5e2a9b87f
prayag <prayagd21@gmail.com>
2020-03-21 19:07:27 +0530

    First draft with filter

--- src/App.js
+++ src/App.js
@@ -6,29 +6,86 @@ class App extends React.Component{
       this.state = {
         value: []
       }
+      this.handleName = this.handleName.bind(this)
+      this.handleCountry = this.handleCountry.bind(this)
     }
+
+  handleName(e){
+    let table = document.getElementById("myTable")
+    let tr = table.getElementsByTagName("tr")
+    for(let i = 0; i < tr.length; i++){
+      let td = tr[i].getElementsByTagName("td")[0];
+      if(td){
+      let txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(e.target.value.toUpperCase()) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+      }
+    }    
+  }
+
+
+  handleCountry(e){
+    let table = document.getElementById("myTable")
+    let tr = table.getElementsByTagName("tr")
+    for(let i = 0; i < tr.length; i++){
+      let td = tr[i].getElementsByTagName("td")[3];
+      if(td){
+      let txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(e.target.value.toUpperCase()) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+      }
+    } 
+  }
 
   componentDidMount(){
     fetch("https://raw.githubusercontent.com/prayagd/unicorn-startups-list/master/src/unicorndata.json")
     .then(res => res.json())
     .then(val => {
       this.setState({
-        value: [val[0], val[1], val[2], val[3]]
+        value: val
       })
     })
   }
 
   render(){
+
     return(
-        <table>
+      <div>
+        <h1>List of Unicorn Startups</h1>
+        <div id = "cover">
+        <div id = "inp">
+          <input type = "text" id = "myInput" placeholder = "Search by name" onChange = {this.handleName}></input>
+          <input type = "text" id = "myInput" placeholder = "Search by country" onChange = {this.handleCountry}></input>
+        </div>
+        <table id = "myTable">
           <tr>
             <th>Startup</th>
-            <th>Valuation</th>
+            <th>Valuation(US$ billion)</th>
             <th>Valuation date</th>
             <th>Country</th>
           </tr>
-    
+          {this.state.value.map((val) => 
+            <tr>
+              <td>{val[0]}</td>
+              <td>{val[1]}</td>
+              <td>{val[2].slice(0, val[2].indexOf("["))}</td>
+              <td>{val[3]}</td>
+            </tr>
+          )}
         </table>
+      </div>
+      <footer>
+        <p>Prayag @ 2020</p>
+        <p>Made with React</p>
+      </footer>
+      </div>
+      
     )
   }
 }
